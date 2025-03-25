47de6295df12273070c136a477ea30dcef70c1fa
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2025-03-25 22:43:17 +0200

    Forge dataset. Plot it

---
+++ example_two_class_classification_dataset_forge.py
@@ -0,0 +1,14 @@
+import mglearn
+import matplotlib.pyplot as plt
+
+# generate dataset. X is the data, y is the labels/classes
+X, y = mglearn.datasets.make_forge()
+# plot dataset
+mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
+plt.legend(["Class 0", "Class 1"], loc=4)
+plt.xlabel("First feature")
+plt.ylabel("Second feature")
+# plt.show()
+plt.savefig("forge_dataset.png")
+print("X.shape: {}".format(X.shape)) # (26, 2), i.e. 26 samples and 2 features
+print("y.shape: {}".format(y.shape)) # (26,), i.e. 26 samples
