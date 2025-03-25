e52d46db5f4bac142a40f7097b4fd3dda567cbca
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2025-03-25 22:49:08 +0200

    Example with regression synthetic wave dataset

---
+++ example_regression_synthetic_wave_dataset.py
@@ -0,0 +1,11 @@
+import mglearn
+import matplotlib.pyplot as plt
+
+X, y = mglearn.datasets.make_wave(n_samples=40)
+plt.plot(X, y, 'o')
+plt.ylim(-3, 3)
+plt.xlabel("Feature")
+plt.ylabel("Target")
+plt.savefig("wave_dataset.png")
+print("X.shape: {}".format(X.shape)) # 40 samples, 1 feature each
+print("y.shape: {}".format(y.shape))
