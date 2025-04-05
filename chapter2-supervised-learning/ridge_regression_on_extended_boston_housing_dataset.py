a210f7d28223c2ad2c7e25414be77522132cff1f
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2025-04-05 12:17:17 +0300

    Ridge regression on boston data using alpha regularization equal to 0.1

--- chapter2-supervised-learning/ridge_regression_on_extended_boston_housing_dataset.py
+++ chapter2-supervised-learning/ridge_regression_on_extended_boston_housing_dataset.py
@@ -6,14 +6,15 @@ from sklearn.linear_model import Ridge
 X, y = mglearn.datasets.load_extended_boston()
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
-rr = Ridge(alpha=1.0)  # alpha is the regularization strength
+rr = Ridge(alpha=0.1)  # alpha is the regularization strength
 
 rr.fit(X_train, y_train)
 
 score_on_training_set = rr.score(X_train, y_train)
-print("Training set R^2 score: {:.2f}".format(score_on_training_set)) # 0.89
+print("Training set R^2 score: {:.2f}".format(score_on_training_set)) # 0.93
 
 score_on_test_set = rr.score(X_test, y_test)
-print("Test set R^2 score: {:.2f}".format(score_on_test_set)) # 0.75
+print("Test set R^2 score: {:.2f}".format(score_on_test_set)) # 0.77
 
-# This is much less discrepancy between training and test set scores.
+discrepancy = score_on_training_set - score_on_test_set
+print("Discrepancy: {:.2f}".format(discrepancy)) # 0.16
